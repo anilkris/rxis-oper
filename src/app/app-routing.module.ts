756f435a3c1a79db08ae8445463d56c2ef069249
anil <anilkrishna@gmail.com>
2020-03-15 00:08:40 +0530

    updated the themes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OperatorsComponent } from './operators/operators.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'operators',
+    component: OperatorsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  HomeComponent, OperatorsComponent
+];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rxjs-oper';
+  //selectedTheme = 'deeppurple-amber';
+
+  selectedTheme = 'purple-green';
+/*
+deeppurple-amber.css
+indigo-pink.css
+pink-bluegrey.css
+purple-green.css
+*/
+  onFirstTheme() {
+    this.selectedTheme = 'deeppurple-amber';
+
+    console.log(this.selectedTheme);
+  }
+  onSecondTheme() {
+    this.selectedTheme = 'indigo-pink';
+
+    console.log(this.selectedTheme);
+  }
+  onThirdTheme() {
+    this.selectedTheme = 'pink-bluegrey';
+
+    console.log(this.selectedTheme);
+  }
+  onFourthTheme() {
+    this.selectedTheme = 'purple-green';
+
+    console.log(this.selectedTheme);
+  }
+
+
+
+
+
+
+
 }
